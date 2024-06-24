8c319552ee22fe1f3b200074367f90d40eb6066d
P4C-M4N <thomassilvestre31@gmail.com>
2024-06-24 17:36:14 +0200

    fix and proprifie

--- solveur.py
+++ solveur.py
@@ -2,6 +2,9 @@ import gurobipy as gp
 from gurobipy import GRB
 import pandas as pd
 
+prefixes = ['fred', 'mary', 'myriame', 'sandra', 'jerome', 'BMP']
+levels = ['6eme', '5eme', '4eme', '3eme']
+
 # Define the number of hours for each level
 nbH = {
     '6eme': 31.5,
@@ -18,14 +21,18 @@ vol_h = {
     '3eme': 4
 }
 
+bmp_nb_grp = 3
+bmp_levels_allow = ['6eme', '5eme']
+
+bmp_levels_forbidden = [level for level in levels if level not in bmp_levels_allow]
+
 # Initialize the model
 model = gp.Model("Repartition Problem")
 
 # Define the variables
 variables = {}
 k_variables = {}
-prefixes = ['fred', 'TP1', 'TP2', 'TP3', 'TP4', 'BMP']
-levels = ['6eme', '5eme', '4eme', '3eme']
+
 
 for prefix in prefixes:
     for level in levels:
@@ -42,10 +49,10 @@ for prefix in prefixes:
         model.addConstr(variables[var_name] == k_variables[k_var_name] * vol_h[level], var_name)
 
 # # Add constraints for total hours per level
-model.addConstr(gp.quicksum(k_variables[f"k_{prefix}_6eme"] for prefix in prefixes) == 7, f"total_6eme")
-model.addConstr(gp.quicksum(k_variables[f"k_{prefix}_5eme"] for prefix in prefixes) == 9, f"total_5eme")
-model.addConstr(gp.quicksum(k_variables[f"k_{prefix}_4eme"] for prefix in prefixes) == 6, f"total_4eme")
-model.addConstr(gp.quicksum(k_variables[f"k_{prefix}_3eme"] for prefix in prefixes) == 5, f"total_3eme")
+model.addConstr(gp.quicksum(k_variables[f"k_{prefix}_6eme"] for prefix in prefixes) == nbH['6eme']/vol_h['6eme'], f"total_6eme")
+model.addConstr(gp.quicksum(k_variables[f"k_{prefix}_5eme"] for prefix in prefixes) == nbH['5eme']/vol_h['5eme'], f"total_5eme")
+model.addConstr(gp.quicksum(k_variables[f"k_{prefix}_4eme"] for prefix in prefixes) == nbH['4eme']/vol_h['4eme'], f"total_4eme")
+model.addConstr(gp.quicksum(k_variables[f"k_{prefix}_3eme"] for prefix in prefixes) == nbH['3eme']/vol_h['3eme'], f"total_3eme")
 
 
 
@@ -54,68 +61,82 @@ for prefix in prefixes:
     model.addConstr(k_variables[f"k_{prefix}_6eme"] <= 2, f"max_6eme_{prefix}")
     model.addConstr(k_variables[f"k_{prefix}_5eme"] <= 2, f"max_5eme_{prefix}")
 
-# Define maximum and minimum hours constraints for each prefix
+# Define maximum and minimum hours constraints for each professor
+
+# Fred
 model.addConstr(gp.quicksum(variables[f"fred_{level}"] for level in levels) <= 17, "fred_max")
 model.addConstr(gp.quicksum(variables[f"fred_{level}"] for level in levels) >= 15, "fred_min")
+model.addConstr(k_variables[f"k_fred_3eme"] >= 1, "fred 3eme yes")
 
-for prefix in ['TP1', 'TP2', 'TP3', 'TP4']:
-    model.addConstr(gp.quicksum(variables[f"{prefix}_{level}"] for level in levels) <= 20, f"{prefix}_max")
-    model.addConstr(gp.quicksum(variables[f"{prefix}_{level}"] for level in levels) >= 18, f"{prefix}_min")
+# Mary
+model.addConstr(gp.quicksum(variables[f"mary_{level}"] for level in levels) <= 20, "mary_max")
+model.addConstr(gp.quicksum(variables[f"mary_{level}"] for level in levels) >= 18, "mary_min")
 
-model.addConstr(k_variables[f"k_BMP_3eme"] == 0, "BMP_3eme_NO")
-model.addConstr(k_variables[f"k_BMP_4eme"] == 0, "BMP_3eme_NO")
-model.addConstr(k_variables[f"k_BMP_5eme"]+k_variables[f"k_BMP_6eme"] == 3, "BMP_5eme_6eme")
+# Myriame
+model.addConstr(gp.quicksum(variables[f"myriame_{level}"] for level in levels) <= 20, "myriame_max")
+model.addConstr(gp.quicksum(variables[f"myriame_{level}"] for level in levels) >= 18, "myriame_min")
+model.addConstr(k_variables[f"k_myriame_3eme"] == 0, "myriame 3eme no")
 
-# Define binary variables to indicate if TP2 teaches at a level
-binary_FRED = {level: model.addVar(vtype=GRB.BINARY, name=f"binary_FRED_{level}") for level in levels}
+# Sandra
+model.addConstr(gp.quicksum(variables[f"sandra_{level}"] for level in levels) <= 20, "sandra_max")
+model.addConstr(gp.quicksum(variables[f"sandra_{level}"] for level in levels) >= 18, "sandra_min")
+
+# Jerome
+model.addConstr(gp.quicksum(variables[f"jerome_{level}"] for level in levels) <= 20, "jerome_max")
+model.addConstr(gp.quicksum(variables[f"jerome_{level}"] for level in levels) >= 18, "jerome_min")
+
+# BMP
+model.addConstr(k_variables[f"k_BMP_{bmp_levels_forbidden[0]}"] == 0, "BMB_contrainte_no_grp_1")
+model.addConstr(k_variables[f"k_BMP_{bmp_levels_forbidden[1]}"] == 0, "BMB_contrainte_no_grp_2")
+model.addConstr(k_variables[f"k_BMP_{bmp_levels_allow[0]}"]+k_variables[f"k_BMP_{bmp_levels_allow[1]}"] == bmp_nb_grp, "BMP_grp_yes")
+
+
+
+
+
+
+# Define binary variables to indicate if professors teaches at a level
+binary_fred = {level: model.addVar(vtype=GRB.BINARY, name=f"binary_fred_{level}") for level in levels}
+binary_mary = {level: model.addVar(vtype=GRB.BINARY, name=f"binary_mary_{level}") for level in levels}
+binary_myriame = {level: model.addVar(vtype=GRB.BINARY, name=f"binary_myriame_{level}") for level in levels}
+binary_sandra = {level: model.addVar(vtype=GRB.BINARY, name=f"binary_sandra_{level}") for level in levels}
+binary_jerome = {level: model.addVar(vtype=GRB.BINARY, name=f"binary_jerome_{level}") for level in levels}
+binary_BMP = {level: model.addVar(vtype=GRB.BINARY, name=f"binary_BMP_{level}") for level in levels}
 
 # Link binary variables with the actual teaching hours
 for level in levels:
-    model.addConstr(k_variables[f"k_fred_{level}"] <= 4 * binary_FRED[level], f"link_FRED_{level}")
+    model.addConstr(k_variables[f"k_fred_{level}"] <= 100 * binary_fred[level], f"link_fred_{level}")
+    model.addConstr(k_variables[f"k_fred_{level}"] >= 0.1 * binary_fred[level], f"link_fred_{level}")
 
-# Ensure TP2 teaches at exactly two levels
-model.addConstr(gp.quicksum(binary_FRED[level] for level in levels) == 2, "FRED_two_levels")
+    model.addConstr(k_variables[f"k_mary_{level}"] <= 100 * binary_mary[level], f"link_mary_{level}")
+    model.addConstr(k_variables[f"k_mary_{level}"] >= 0.1 * binary_mary[level], f"link_mary_{level}")
 
-model.addConstr(k_variables[f"k_fred_3eme"] >= 1, "fred 3eme oui")
+    model.addConstr(k_variables[f"k_myriame_{level}"] <= 100 * binary_myriame[level], f"link_myriame_{level}")
+    model.addConstr(k_variables[f"k_myriame_{level}"] >= 0.1 * binary_myriame[level], f"link_myriame_{level}")
 
-# Define binary variables to indicate if TP2 teaches at a level
-binary_TP2 = {level: model.addVar(vtype=GRB.BINARY, name=f"binary_TP2_{level}") for level in levels}
+    model.addConstr(k_variables[f"k_sandra_{level}"] <= 100 * binary_sandra[level], f"link_sandra_{level}")
+    model.addConstr(k_variables[f"k_sandra_{level}"] >= 0.1 * binary_sandra[level], f"link_sandra_{level}")
 
-# Link binary variables with the actual teaching hours
-for level in levels:
-    model.addConstr(k_variables[f"k_TP2_{level}"] <= 5 * binary_TP2[level], f"link_TP2_{level}")
+    model.addConstr(k_variables[f"k_jerome_{level}"] <= 100 * binary_jerome[level], f"link_jerome_{level}")
+    model.addConstr(k_variables[f"k_jerome_{level}"] >= 0.1 * binary_jerome[level], f"link_jerome_{level}")
 
-# Ensure TP2 teaches at exactly two levels
-model.addConstr(gp.quicksum(binary_TP2[level] for level in levels) == 2, "TP2_two_levels")
+    model.addConstr(k_variables[f"k_BMP_{level}"] <= 100 * binary_BMP[level], f"link_BMP_{level}")
+    model.addConstr(k_variables[f"k_BMP_{level}"] >= 0.1 * binary_BMP[level], f"link_BMP_{level}")
 
-# Define binary variables to indicate if TP3 teaches at a level
-binary_TP3 = {level: model.addVar(vtype=GRB.BINARY, name=f"binary_TP3_{level}") for level in levels}
-
-# Link binary variables with the actual teaching hours
-for level in levels:
-    model.addConstr(k_variables[f"k_TP3_{level}"] <= 5 * binary_TP3[level], f"link_TP3_{level}")
-
-# Ensure TP3 teaches at exactly two levels
-model.addConstr(gp.quicksum(binary_TP3[level] for level in levels) == 2, "TP3_two_levels")
-
-# # Define binary variables to indicate if TP4 teaches at a level
-# binary_TP4 = {level: model.addVar(vtype=GRB.BINARY, name=f"binary_TP4_{level}") for level in levels}
-
-# # Link binary variables with the actual teaching hours
-# for level in levels:
-#     model.addConstr(k_variables[f"k_TP4_{level}"] <= 5 * binary_TP4[level], f"link_TP4_{level}")
-
-# # Ensure TP4 teaches at exactly two levels
-# model.addConstr(gp.quicksum(binary_TP4[level] for level in levels) == 2, "TP4_two_levels")
-
-
-model.addConstr(k_variables[f"k_TP4_4eme"] == 1, "TP4 3eme oui")
-
+# Setup level constraints
+model.addConstr(gp.quicksum(binary_fred[level] for level in levels) == 2, "fred_levels")
+model.addConstr(gp.quicksum(binary_mary[level] for level in levels) <= 3, "mary_levels")
+model.addConstr(gp.quicksum(binary_myriame[level] for level in levels) <= 3, "myriame_levels")
+model.addConstr(gp.quicksum(binary_sandra[level] for level in levels) <= 3, "sandra_levels")
+#model.addConstr(gp.quicksum(binary_jerome[level] for level in levels) == 2, "jerome_levels")
+#model.addConstr(gp.quicksum(binary_BMP[level] for level in levels) == 2, "BMP_levels")
 
 
 
 # Define the objective function (minimization of the sum of TP variables)
-objective = gp.quicksum(variables[var] for var in variables if "TP" in var and "TP1" not in var)
+prefixes_to_minimize = ['mary', 'myriame', 'jerome']
+
+objective = gp.quicksum(variables[f"{prefix}_{level}"] for prefix in prefixes_to_minimize for level in levels)
 model.setObjective(objective, GRB.MINIMIZE)
 
 # Optimize the model
